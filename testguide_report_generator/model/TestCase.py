41ba1e18ae872c126e956f743112a78ee7c6772d
maxe <Max.Ehrlich@tracetronic.de>
2025-07-01 17:10:17 +0200

    adjust line length (#18)

--- testguide_report_generator/model/TestCase.py
+++ testguide_report_generator/model/TestCase.py
@@ -186,7 +186,10 @@ class Attribute(Json2AtxRepr):
         :param value: Attribute value
         :type value: str
         """
-        str_pattern = "^[-.0-9:A-Z_a-z\u00b7\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u037d\u037f-\u1fff\u200c-\u200d\u203f\u2040\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd]+$"
+        str_pattern = (
+            "^[-.0-9:A-Z_a-z\u00b7\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u037d\u037f-\u1fff\u200c-\u200d"
+            "\u203f\u2040\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd]+$"
+        )
         pattern = re.compile(str_pattern)
         if not pattern.match(key):
             raise ValueError(f"Attribute keys need to be structured following this pattern: {str_pattern}")
